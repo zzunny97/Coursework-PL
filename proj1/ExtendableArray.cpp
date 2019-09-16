2710c19f6d90471877f3731040d22720a0492714
zzunny97 <zzunny97@gmail.com>
2019-09-16 16:50:08 +0900

    basic implementation complete, limiting resizing not yet

--- proj1/ExtendableArray.cpp
+++ proj1/ExtendableArray.cpp
@@ -1,1 +1,111 @@
+#include "ExtendableArray.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+ElementRef::ElementRef(ExtendableArray& theArray, int i) {
+	//cout << "1st constructor" << endl;
+	intArrayRef = &theArray;
+	index = i;	
+}
+ElementRef::ElementRef(const ElementRef& other) {
+	//cout << "2nd constructor" << endl;
+	intArrayRef = other.intArrayRef;
+	index = other.index;
+}
+
+ElementRef::~ElementRef() {
+	//cout << "destructor ElmentRef" << endl;
+}
+
+ElementRef& ElementRef::operator=(const ElementRef& rhs) {
+	cout << "hello1" << endl;
+	cout << "index = " << index << endl;
+	cout << "rhs.index = " << rhs.index << endl;
+	intArrayRef->arrayPointer[index] = rhs.intArrayRef->arrayPointer[rhs.index];
+	cout << "\n\n" << endl;
+	return *this;
+}
+
+ElementRef& ElementRef::operator=(int val) {
+	cout << "hello2" << endl;
+	//cout << "2nd ref operatror =" << endl;
+	//cout << val << endl;
+	//cout << index << endl;
+	//cout << intArrayRef->arrayPointer << endl;
+
+	
+	intArrayRef->arrayPointer[index] = val;
+	
+	//cout << "returning" << endl;
+	return *this;
+}
+
+ElementRef::operator int() const {
+	cout << "hello3" << endl;
+	//cout << "3rd ref operatror =" << endl;
+	return intArrayRef->arrayPointer[index];
+}
+
+
+ExtendableArray::ExtendableArray() {
+	arrayPointer = (int*)malloc(sizeof(int)*2);
+	arrayPointer[0] = 0;
+	arrayPointer[1] = 0;
+	size = 2;
+}
+
+ExtendableArray::ExtendableArray(const ExtendableArray& other) {
+	cout << "copying constructor array" << endl;
+	cout << "cur size = " << size << " other size = " << other.size << endl;
+	int len = other.size;
+	delete[] arrayPointer;
+	arrayPointer = (int*)malloc(sizeof(int)*len); 
+	size = len;
+
+	for(int i=0; i<len; i++){
+		arrayPointer[i] = other.arrayPointer[i];
+	}
+}
+
+ExtendableArray::~ExtendableArray() {
+	cout << "destructor ExtendableArray" << endl;
+	//delete[] arrayPointer;
+	cout << "why" << endl;
+}
+
+ExtendableArray& ExtendableArray::operator=(const ExtendableArray& rhs) {
+	int len = rhs.size;
+	delete[] arrayPointer;
+	arrayPointer = (int*)malloc(sizeof(int)*len);
+	size = len;
+
+	for(int i=0; i<len; i++) arrayPointer[i] = rhs.arrayPointer[i];
+
+	//arrayPointer = rhs.arrayPointer;
+	//size = rhs.size;
+
+	return *this;
+}
+
+
+ElementRef ExtendableArray::operator[](int i) {
+	if(i >= size) {
+		//cout << i << " " << size << endl;
+		int old_size = size;
+		int new_size = i+1;
+		arrayPointer = (int*)realloc(arrayPointer, sizeof(int)*(new_size));
+		for(int i=old_size; i<new_size; i++) {
+			arrayPointer[i] = 0;
+		}
+		size = new_size;
+		cout << "resized to " << new_size << endl;
+	}
+	cout << "i = " << i << endl;
+	ElementRef ret(*this, i);
+	cout << "operator[] func end" << endl;	
+	return ret;	
+}
+
 
--- proj1/driver1.cpp
+++ proj1/driver1.cpp
@@ -3,41 +3,55 @@
 using namespace std;
 
 void stuff_20(ExtendableArray arr) {
+	cout << "here is stuff_20 " << endl;
     for (int i=0; i < 20; i++) { 
-        arr[ i ] = i;
+        arr[i] = i;
     }
     cout << arr << endl; // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
 }
 
 int main() { 
     ExtendableArray a1; 
-    for (int i=0;i<20;i++)
+    for (int i=0;i<20;i++) {
         a1[i] = i;
-    cout << a1 << endl; //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
-
+	}
+    cout << a1 << "\n\n" << endl; //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+	
     if(1){
         ExtendableArray a2; 
 
-        for (int i=0;i<10;i++)
+        for (int i=0;i<10;i++){
             a2[i+5] = a1[i];
+			// cout << a2 << endl;
+		}
 
         cout << a2 << endl; //0 0 0 0 0 0 1 2 3 4 5 6 7 8 9
+		cout << "=====test3=====" << endl;
 
         a1 = a2;
+		cout << "test3-1" << endl;
         for (int i=0;i<10;i++) 
             a2[i] = i;
+		cout << "test3-2" << endl;
 
         cout << a1 << endl; //0 0 0 0 0 0 1 2 3 4 5 6 7 8 9
-        cout<< a2 <<endl; //012345678956789 
+        cout<< a2 <<endl; //012345678956789 */
     }
+	
 
+	cout << "zzunny" << endl;
     cout << a1 << endl; //0 0 0 0 0 0 1 2 3 4 5 6 7 8 9 
+	cout << "zzunny2" << endl;
+	
 
     ExtendableArray a3;
     a3[0] = 1;
+	cout << "zzunny3" << endl;
     cout << a3 << endl; // 1 0
     stuff_20(a3);
+	cout << "zzunny4" << endl;
     cout << a3 << endl; // 1 0
+
     cout << a3[2147483647] << endl; // 0
 
     return 0; 
