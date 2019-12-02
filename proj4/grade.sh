60b5cb895c7cd427645cc113115a6de262d7d745
zzunny97 <zzunny97@gmail.com>
2019-12-02 20:16:13 +0900

    .

--- proj4/grade.sh
+++ proj4/grade.sh
@@ -4,6 +4,7 @@
 	echo "Usage: $0 MAXPOINTS";
 	exit 1;
 }
+
 ./clear.sh
 
 maxpoint=$1
@@ -24,20 +25,22 @@ do
 	diff -bwi $id".output" output.txt > $id".diff"
 	
 	diffcnt=`grep -rn '>' $id."diff" | wc -l`
+	diffcnt2=`grep -rn '<' $id."diff" | wc -l`
 #echo diffcnt=$diffcnt
-	((curpoint="${curpoint}"-$diffcnt))	
+	((diffcnt_total="${diffcnt}"+$diffcnt2))
+	((curpoint="${curpoint}"-$diffcnt_total))	
 	if [ ${diffcnt} -eq 0 ]; then
 		echo $i has correct output
 		echo $i has $comments lines with comments
 		echo $i has earned a score of $curpoint / $maxpoint
 	else
-		echo $i has incorrect output "("$diffcnt lines do not match")"
+		echo $i has incorrect output "("$diffcnt_total lines do not match")"
 		echo $i has $comments lines with comments
 		echo $i ahs earned a score of $curpoint / $maxpoint
 
 	fi
-
-	
 done
+
+./clear.sh
 
 
