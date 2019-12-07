b0f99b8ac16c4cd5c74c4e7556312c7765ae1734
zzunny97 <zzunny97@gmail.com>
2019-12-07 18:14:17 +0900

    .

---
+++ proj4/202012347.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	printf("Alice\n");
+	printf("Bob\n");
+	printf("Charlie\n");
+	printf("Dave\n");
+	printf("Eve\n");
+	printf("Frank\n");
+	printf("Grace\n");
+	printf("Heidi\n");
+	printf("Isable\n");
+	printf("Jack\n");
+	printf("Karl\n");
+	printf("Larry\n");
+	printf("Michale\n");
+	printf("Nancy\n");
+	printf("Owen\n");
+	printf("Peter\n");
+	printf("Robert\n");
+	//	// // 
+
+	return 0;
+}
--- proj4/grade.sh
+++ proj4/grade.sh
@@ -45,5 +45,3 @@ do
 done
 
 ./clear.sh
-
-
